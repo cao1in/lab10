d65d79e640bf463b5587e095c2697c8b2c17f146
cao1in <alexana03@mail.ru>
2021-12-17 14:40:09 +0300

    Initial commit

---
+++ src/testAnimal.java
@@ -0,0 +1,13 @@
+public class testAnimal {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog doge = new Dog();
+        doge.run(300);
+        cat.run(300);
+        doge.swim(5);
+        cat.swim(5);
+        doge.jump(1.5f);
+        cat.jump(1.5f);
+    }
+}
+
